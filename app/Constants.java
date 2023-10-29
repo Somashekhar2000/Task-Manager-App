2194e72c812f0821db8bf41c7ba46029675e6b48
Somashekhar <somashekharm4159@gmail.com>
2023-10-29 17:35:35 +0530

    commiting entire task manager app

---
+++ app/Constants.java
@@ -0,0 +1,10 @@
+package com.taskManager.app;
+
+import java.io.File;
+
+public interface Constants {
+	int PRIORITY1 = 1;
+	int PRIORITY2 = 5;
+	int PRIORITY3 = 10;
+	File PATH = new File("D:\\eclipse_new_workspace\\TaskManagerApp\\src\\com\\taskManager\\DetailsOfApp");
+}
